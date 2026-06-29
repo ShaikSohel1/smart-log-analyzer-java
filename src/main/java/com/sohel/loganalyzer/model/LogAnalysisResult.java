8bc024096a3dbd7293fdce65b7bfe2efb99e1930
shaik sohel <shaiksohel@MacBookAir.lan>
2026-06-29 12:27:59 +0530

    Log Analysis Result modifications

--- src/main/java/com/sohel/loganalyzer/model/LogAnalysisResult.java
+++ src/main/java/com/sohel/loganalyzer/model/LogAnalysisResult.java
@@ -4,20 +4,36 @@ import java.util.Map;
 
 public class LogAnalysisResult {
 
-    private int totalLogs;
-    private int errors;
-    private int failedLogins;
-    private Map<String, Integer> suspiciousIPs;
+    private final int totalLogs;
+    private final int errors;
+    private final int failedLogins;
+    private final Map<String, Integer> suspiciousIPs;
 
-    public LogAnalysisResult(int totalLogs, int errors, int failedLogins, Map<String, Integer> suspiciousIPs) {
+    public LogAnalysisResult(
+            int totalLogs,
+            int errors,
+            int failedLogins,
+            Map<String, Integer> suspiciousIPs) {
+
         this.totalLogs = totalLogs;
         this.errors = errors;
         this.failedLogins = failedLogins;
         this.suspiciousIPs = suspiciousIPs;
     }
 
-    public int getTotalLogs() { return totalLogs; }
-    public int getErrors() { return errors; }
-    public int getFailedLogins() { return failedLogins; }
-    public Map<String, Integer> getSuspiciousIPs() { return suspiciousIPs; }
+    public int getTotalLogs() {
+        return totalLogs;
+    }
+
+    public int getErrors() {
+        return errors;
+    }
+
+    public int getFailedLogins() {
+        return failedLogins;
+    }
+
+    public Map<String, Integer> getSuspiciousIPs() {
+        return suspiciousIPs;
+    }
 }
